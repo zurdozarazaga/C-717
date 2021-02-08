cf23235e2e41bea017a073d5834b3a07fe56eb3c
Emanuel Zarazaga <zurdozarazaga@gmail.com>
2021-02-08 20:35:19 -0300

    primer commit despues de que se instalo ubuntu. Sin paquetes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,47 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../assets/styles/components/Navigation.scss';
 
-const Navigation = () => (
-  <nav className='navigation'>
-    <ul className='navigation__container'>
-      <li className='navigation__item--usuario'>
-        Usuario
-      </li>
-      <li className='navigation__item--Inicio'>
-        <a href='./'>Inicio</a>
-      </li>
-      <div className='navigationEdit__container'>
-        <li className='navigation__item--Editar'>
-          <a href='#editar'>Editar</a>
-          <ul id='editar'>
-            <li className='navigation__item--Montaje'>
-              <a href='./'>Montaje</a>
-            </li>
-            <li className='navigation__item--Desmontaje'>
-              <a href='./'>Desmontaje</a>
-            </li>
-          </ul>
-        </li>
-      </div>
-      <li className='navigation__item--Elementos'>
-        <a href='./'>Elementos</a>
-        <ul>
-          <li className='navigation__item--Activos'>
-            <a href='./'>Activos</a>
-          </li>
-          <li className='navigation__item--Vencimientos'>
-            <a href='./'>Vencimientos</a>
-          </li>
-        </ul>
-      </li>
-      <li className='navigation__item--Contactos'>
-        <a href='./'>Contactos</a>
-      </li>
-      <li className='navigation__item--Configuración'>
-        <a href='./'>Configuración</a>
-      </li>
-    </ul>
-  </nav>
-);
+function usedropdown(e) {
+  const [state, setState] = React.useState();
+  if (state === e) {
+    setState('visible');
+  } else {
+    setState('no-visible');
 
+    return [state, setState];
+  }
+}
+
+const Navigation = () => {
+  const [state, setState] = usedropdown('no-visible');
+  return (
+    <nav className='navigation'>
+      <ul className='navigation__container'>
+        <li className='navigation__item--usuario'>
+          Usuario
+        </li>
+        <li className='navigation__item--Inicio'>
+          <a href='./'>Inicio</a>
+        </li>
+        <li className='navigation__item--Editar' id='editar'>
+          <a href='#editar'>Editar</a>
+          <div onClick={ ()=> { 
+            setState(state)
+          }} className='submenu'>
+            <a href='./'>Montaje</a>
+            <a href='./'>Desmontaje</a>
+          </div>
+        </li>
+        <li className='navigation__item--Editar' id='editar'>
+          <a href='#editar'>Elementos</a>
+          <div className='submenu'>
+            <a href='./'>Activos</a>
+            <a href='./'>Vencimientos</a>
+          </div>
+        </li>
+        <li className='navigation__item--Contactos'>
+          <a href='./'>Contactos</a>
+        </li>
+        <li className='navigation__item--Configuración'>
+          <a href='./'>Configuración</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 export default Navigation;

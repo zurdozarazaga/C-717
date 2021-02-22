d5c64befd2d94d8e7acc48fda80f9085f9882bb6
Emanuel Zarazaga <zurdozarazaga@gmail.com>
2021-02-21 23:32:39 -0300

    se agrego el sprip deploy a package y se comenzo a conectar la app a la base de datos

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/firebase-app";
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAO8nr3BE_SN1ffBZaUqodo9_WRy7l9hGw",
+    authDomain: "c-717-58cb9.firebaseapp.com",
+    projectId: "c-717-58cb9",
+    storageBucket: "c-717-58cb9.appspot.com",
+    messagingSenderId: "434164082676",
+    appId: "1:434164082676:web:3ef6e59ac3b022f4adbba1",
+    measurementId: "G-SXY9NM26FH"
+  };
+// Initialize Firebase
+const fb = firebase.initializeApp(firebaseConfig);
+
+const db = fb.firestore();
+
+export default db;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 
+import './firebase';
+
 ReactDOM.render(<App />, document.getElementById('app'));

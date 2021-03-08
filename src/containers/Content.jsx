4030bda525f92e9fb2942e27114eacc247a53bbd
Emanuel Zarazaga <zurdozarazaga@gmail.com>
2021-03-08 19:36:04 -0300

    se cambio la llamada a la base de datos en content

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../assets/styles/components/Content.scss';
 import Montajes from '../components/Montajes';
 import Desmontajes from '../components/Desmontajes';
 import Activos from '../components/Activos';
 import Vencimientos from '../components/Vencimientos';
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import db from '../firebase';
 
-const Content = () => (
-  <section className='content'>
-    <Montajes />
-    <Desmontajes />
-    <Activos />
-    <Vencimientos />
-  </section>
+const Content = () => {
+  const [data, setData] = useState([]);
+  const db = firebase.firestore();
+  useEffect(() => {
+    db.collection('Elementos').get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(doc.data());
+        // console.log(`${doc.id} => ${doc.data()}`);
+      });
+    });
+  });
+  return (
+    <section className='content'>
+      <Montajes />
+      <Desmontajes />
+      <Activos />
+      <Vencimientos />
+    </section>
 
-);
+  );
+};
 
 export default Content;

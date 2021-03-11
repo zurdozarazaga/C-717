4764ca0872aa7cd7957ad7d6dde358525d24a731
Emanuel Zarazaga <zurdozarazaga@gmail.com>
2021-03-11 13:47:28 -0300

    se pudo obtener los elementos de la base de datos y se lo pinto en montajes

--- src/components/Montajes.jsx
+++ src/components/Montajes.jsx
@@ -1,43 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../assets/styles/Montajes.scss';
+import firebase from "firebase/app";
+import 'firebase/firestore';
 import db from '../firebase';
 
 const Montajes = () => {
 
-
-
-
   const [data, setData] = useState([]);
-
+  React.useEffect(() => {
+    const getData = async () => {
+      const db = firebase.firestore();
+      try {
+        const data = await db.collection('Elementos').get();
+        const arrayData = data.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        // console.log(arrayData);
+        setData(arrayData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
   return (
-    <table summary="Los grupos de música punk más famosos del Reino Unido">
-      <caption>Un resumen de los grupos de música punk más famosos del Reino Unido</caption>
+    <table summary='Elementos montados en la aeronave'>
+      <caption>Elementos montados en la aeronave</caption>
       <thead>
         <tr>
-          <th scope="col">Número de Parte</th>
           <th scope="col">Designación</th>
+          <th scope="col">Número de Parte</th>
           <th scope="col">Número de Serie</th>
           <th scope="col">Posición</th>
+          <th scope="col">Número de Desmontaje</th>
           <th scope="col">Fecha</th>
         </tr>
       </thead>
       <tbody>
-        {data.map(elemento=>(
-          <tr>
-            <th scope="row">{elemento.id}</th>
-            <td>{elemento.nombre}</td>
-            <td>{elemento.minutos}</td>
-            <td>
-              <button className='' onClick={()=>seleccionarPais(elemento, 'Editar')}>Editar</button> {"   "} 
-              <button className='' onClick={()=>seleccionarPais(elemento, 'Eliminar')}>Eliminar</button>
-            </td>
-          </tr>
-        ))}
+        {
+          data.map((elemento, i) => {
+            return (
+              <tr>
+                <td key={i}>{ elemento.designacion }</td>
+                <th scope="row">{ elemento.numeroParte }</th>
+                <td >{ elemento.numeroSerie }</td>
+                <td >{ elemento.posicion }</td>
+                <td >{ elemento.numeroDesmontaje }</td>
+                <td > fecha </td>
+              </tr>
+            );
+          })
+        }
       </tbody>
     </table>
   );
 };
-
-
-
 export default Montajes;

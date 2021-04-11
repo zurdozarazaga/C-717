eb4e27a77f6ceb4e9b9a5445888b3c9a911ba207
Emanuel Zarazaga <zurdozarazaga@gmail.com>
2021-04-11 17:28:38 -0300

    rama para la implementacion del modal

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import '../assets/styles/components/Add.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Add = () => (
   <>
-    
-    <botton className='add--botton'>
-    <label className='add--label'>Nuevo:</label>
+    <a href='#mimodal' className='add--botton'>
+      <label className='add--label'>Nuevo:</label>
       <FontAwesomeIcon icon={faPlusSquare} />
-    </botton>
+    </a>
   </>
 );
 

0c10de8fbb777c1221ac7f8faba10583f54a3f4e
Emanuel Zarazaga <zurdozarazaga@gmail.com>
2021-05-17 23:38:16 -0300

    se comenzara a implementar el Redux en otra rama

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import App from './routes/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './firebase';
+
+const store = createStore(
+  {}, // todos los reducers
+  {}, // estados iniciales
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));

0ab6bce88f87273ec71ee42b9335fe6e71711639
Emanuel Zarazaga <zurdozarazaga@gmail.com>
2021-05-27 23:56:36 -0300

    se implemento redux en montaje

--- src/index.js
+++ src/index.js
@@ -4,14 +4,23 @@ import App from './routes/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+
+import reducers from './reducers';
 
 import './firebase';
 
 const store = createStore(
-  {}, // todos los reducers
+  reducers, // todos los reducers
   {}, // estados iniciales
+  applyMiddleware(reduxThunk),
 );
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app'),
+);

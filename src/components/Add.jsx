a945337e7c296a6736028f66aca8bb6e9f273996
zurdozarzaga <zurdozarazaga@gmail.com>
2021-07-01 21:40:32 -0300

    se agrego el filter al component montajes pero se debe acomodar la tabla

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -6,42 +6,42 @@ import Modal from './Modal';
 // import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Input, Label } from 'reactstrap';
 
 const Add = (props) => {
-  const { getCurrentId } = props;
-  const [newElement, setNewElement] = useState(false);
+  // const { getCurrentId } = props;
+  // const [newElement, setNewElement] = useState(false);
 
-  const currentId = getCurrentId();
-  console.log(currentId);
+  // const currentId = getCurrentId();
+  // console.log(currentId);
 
-  const functionEdit = (elementoId, currentId) => {
-    () => setCurrentId(elementoId);
-    console.log(currentId);
-    if (newElement === false && currentId === undefined) {
-      setNewElement(!newElement);
-    } else if (newElement === true && currentId === undefined) {
-      setNewElement(!newElement);
-    } else if (newElement === false && currentId !== undefined) {
-      console.log('editando');
-    };
-  };
+  // const functionEdit = (elementoId, currentId) => {
+  //   () => setCurrentId(elementoId);
+  //   console.log(currentId);
+  //   if (newElement === false && currentId === undefined) {
+  //     setNewElement(!newElement);
+  //   } else if (newElement === true && currentId === undefined) {
+  //     setNewElement(!newElement);
+  //   } else if (newElement === false && currentId !== undefined) {
+  //     console.log('editando');
+  //   };
+  // };
 
-  const handleClickNewElement = () => {
-    console.log(currentId);
-    if (newElement === false && currentId === undefined) {
-      setNewElement(!newElement);
-    } else if (newElement === true && currentId === undefined) {
-      setNewElement(!newElement);
-    } else if (newElement === false && currentId !== undefined) {
-      console.log('editando');
-    };
-  };
+  // const handleClickNewElement = () => {
+  //   console.log(currentId);
+  //   if (newElement === false && currentId === undefined) {
+  //     setNewElement(!newElement);
+  //   } else if (newElement === true && currentId === undefined) {
+  //     setNewElement(!newElement);
+  //   } else if (newElement === false && currentId !== undefined) {
+  //     console.log('editando');
+  //   };
+  // };
   return (
     <>
       <label className='add--label'>Nuevo:</label>
-      <a href='#mimodal' onClick={handleClickNewElement} className='add--botton'>
+      <a href='#mimodal'  className='add--botton'>
         <FontAwesomeIcon icon={faPlusSquare} />
       </a>
 
-      <Modal newElement={newElement} onClick={handleClickNewElement} getCurrentId={getCurrentId} />
+      <Modal  />
     </>
   );
 };
--- src/components/Montajes.jsx
+++ src/components/Montajes.jsx
@@ -18,40 +18,43 @@ const Montajes = (props) => {
 
   console.log(props);
   return (
-    <div className='table__container'>
-      <table summary='Elementos montados en la aeronave'>
-        <caption>Elementos montados en la aeronave</caption>
-        <thead>
-          <tr>
-            <th scope="col">Designación</th>
-            <th scope="col">Número de Parte</th>
-            <th scope="col">Número de Serie</th>
-            <th scope="col">Posición</th>
-            <th scope="col">Número de Desmontaje</th>
-            <th scope="col">Fecha</th>
-            <th scope="col"> Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* recorre el array del query */}
-          {
-            props.elementos.map((elemento, i) => {
-              return (
-                <tr>
-                  <td className='td__designacion' key={i}>{ elemento.designacion }</td>
-                  <th className='td__numeroParte' scope="row">{ elemento.numeroParte }</th>
-                  <td className='td__numeroSerie'>{ elemento.numeroSerie }</td>
-                  <td className='td__posicion'>{ elemento.posicion }</td>
-                  <td className='td__numeroDesmontaje'>{ elemento.numeroDesmontaje }</td>
-                  <td className='td__fecha'> fecha </td>
-                  <td className='td__acciones' />
-                </tr>
-              );
-            })
-          }
-        </tbody>
-      </table>
-    </div>
+    <>
+      <Filter />
+      <div className='table__container'>
+        <table summary='Elementos montados en la aeronave'>
+          <caption>Elementos montados en la aeronave</caption>
+          <thead>
+            <tr>
+              <th scope="col">Designación</th>
+              <th scope="col">Número de Parte</th>
+              <th scope="col">Número de Serie</th>
+              <th scope="col">Posición</th>
+              <th scope="col">Número de Desmontaje</th>
+              <th scope="col">Fecha</th>
+              <th scope="col"> Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* recorre el array del query */}
+            {
+              props.elementos.map((elemento, i) => {
+                return (
+                  <tr>
+                    <td className='td__designacion' key={i}>{ elemento.designacion }</td>
+                    <th className='td__numeroParte' scope="row">{ elemento.numeroParte }</th>
+                    <td className='td__numeroSerie'>{ elemento.numeroSerie }</td>
+                    <td className='td__posicion'>{ elemento.posicion }</td>
+                    <td className='td__numeroDesmontaje'>{ elemento.numeroDesmontaje }</td>
+                    <td className='td__fecha'> fecha </td>
+                    <td className='td__acciones' />
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
